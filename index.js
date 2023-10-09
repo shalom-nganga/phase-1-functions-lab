61ef19d63c586397a4bd8e4bd14d36bedc5606aa
shalom-nganga <minnomacharia@gmail.com>
2023-10-09 08:03:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Code your solution in this file!
+
+let distanceFromHqInBlocks=function (somevalue){
+    if (somevalue>42){
+        return somevalue-42
+    } else
+        return Math.abs(somevalue-42);
+
+}
+
+function distanceFromHqInFeet(somevalue) {
+console.log(distanceFromHqInBlocks)(somevalue);
+return distanceFromHqInBlocks(somevalue) *264;
+}
+
+function distanceTravelledInFeet(start,destination){
+    if (destination>42){
+        let feet =((destination-start) *264)
+        return feet
+    } else {
+        let feet =((start-destination) *264)
+        return Math.abs(feet)
+    }
+}
+
+function calculatesFarePrice(start,destination){
+let distance=distanceTravelledInFeet(start,destination)
+let fare=0
+if (distance <= 400){
+    return fare
+}
+
+else if (distance >2000 && distance <=2499){
+    let fare=25
+    return fare
+}
+else if (distance >2000 && distance <=2499){
+
+    let fare=25
+    return fare
+}
+else if (distance>=2500){
+    return "cannot travel that far"
+}
+
+}
+console.log(calculatesFarePrice(4,11))
+
+
+
+
+
+
+
+
+
+
